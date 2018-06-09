91c67484f345d1ec2f4a3f259d5a263ecaf3a968
DamianoSchirinzi <Damiano16@hotmail.co.uk>
2018-06-09 13:17:49 +0100

    Added Moving Traps

---
+++ Isometric Project/Assets/Scripts/MovingTrap.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTrap : MonoBehaviour {
+
+    //SET TO POSITIONS USING WAYPOINTS
+    public Transform movingTrap;
+    public Transform position1;
+    public Transform position2;
+    public Vector3 newPosition;
+    public string currentState;
+    public float smooth;
+    public float resetTime;
+
+    //DIFFERENT SPEED AND DAMAGE VARIABLES
+    
+    private void Start()
+    {
+        ChangeTarget();
+    }
+
+    private void FixedUpdate()
+    {
+        movingTrap.position = Vector3.Lerp(movingTrap.position, newPosition, smooth * Time.deltaTime);
+    }
+
+    void ChangeTarget()
+    {
+        if(currentState == "Moving To Position 1")
+        {
+            currentState = "Moving To Position 2";
+            newPosition = position2.position;
+        }
+        else if (currentState == "Moving To Position 2")
+        {
+            currentState = "Moving To Position 1";
+            newPosition = position1.position;
+        }
+        else if (currentState == "")
+        {
+            currentState = "Moving To Position 2";
+            newPosition = position2.position;
+        }
+        Invoke("ChangeTarget", resetTime);
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            print("Player Detected");
+        }
+    }
+}

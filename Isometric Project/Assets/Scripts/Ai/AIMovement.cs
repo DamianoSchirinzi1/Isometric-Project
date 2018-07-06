4041447250bc723994a543b9b7849c047877f886
DamianoSchirinzi <Damiano16@hotmail.co.uk>
2018-07-06 17:53:22 +0100

    Added AI Movement

---
+++ Isometric Project/Assets/Scripts/Ai/AIMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour {
+
+    //Waypoint variables
+    public Transform[] patrolPoints;
+    private int currentPatrolIndex;
+    public int specificPatrolIndex;
+    Transform currentPatrolPoint;
+    
+    public float speed;
+
+    SpriteRenderer spriteRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentPatrolIndex = Random.Range(0, patrolPoints.Length);
+        currentPatrolPoint = patrolPoints[currentPatrolIndex];
+              
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 patrolPointDir = currentPatrolPoint.position - transform.position;
+        //Moves the AI up
+        transform.Translate(patrolPointDir * Time.deltaTime * speed);
+        
+        //Check if AI has reached its waypoint
+        if(Vector2.Distance(transform.position, currentPatrolPoint.position) < .1f)
+        {
+            //Reached patrol point            
+            if (currentPatrolIndex + 1 < patrolPoints.Length)
+            {
+                //If the index is more than the patrol points length
+                //Check to see if their are any more patrol points, if not, go back to the beginning
+                currentPatrolIndex++;
+            }
+            else
+            {
+                currentPatrolIndex = 0;
+            }
+            currentPatrolPoint = patrolPoints[currentPatrolIndex];
+        }
+
+        // Turn towards current patrol point, find the direction the patrol point is in
+        
+        //uses the direction of the patrol point to calculate an angle
+        float angle = Mathf.Atan2(patrolPointDir.y, patrolPointDir.x) * Mathf.Rad2Deg;
+        print(angle);
+        if(angle > -100f)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }        
+    }
+}
---
+++ Isometric Project/Assets/Scripts/Player/ColliderDetection.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderDetection : MonoBehaviour
+{
+
+    CharController CC;
+    int layerMask;
+
+    GameObject walls;
+    //GameObject[] obstacles;
+
+    private void Start()
+    {
+        layerMask = ~(LayerMask.GetMask("Obstacle"));
+        CC = GetComponent<CharController>();
+
+        walls = GameObject.FindGameObjectWithTag("Walls");
+        //obstacles = GameObject.FindGameObjectsWithTag("Obstacles");
+    }
+
+    void FixedUpdate()
+    {
+        RaycastObjects();
+    }
+
+    void RaycastObjects()
+    {
+        //Creating a raycast from the players position which points in the players current direction
+        //DrawRay paints a ray which follows the raycast i created.
+        Debug.DrawRay(transform.position, CC.direction, Color.red);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, CC.direction, 1);
+
+        if (hit.collider == null)
+        {
+            return;
+        }
+
+        //Checks if the raycast has detected a wall
+        if (hit.collider.gameObject == walls)
+        {
+            Debug.Log("Raycast : " + hit.collider.gameObject.tag);
+            //Checks the distance from the origin of the raycast too the hit game object. If below the number specified it will excute the code below.
+            if (hit.distance <= 0.08f)
+            {
+                //Provides negative force to push the player away from the wall.
+                print("Player is nearing an obstacle = " + hit.distance);
+                transform.Translate(-CC.direction * 1.25f * Time.deltaTime);
+            }
+        }
+
+        if (hit.collider.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("Raycast : " + hit.collider.gameObject.tag);
+            //Checks if the player is currently phasing
+            if (hit.distance <= 0.08f && CC.phasing != true)
+            {
+                print("Player is nearing an obstacle = " + hit.distance);
+                transform.Translate(-CC.direction * 1.25f * Time.deltaTime);
+            }
+            //If they are not phasing, the will not be pushed away from the object
+            else if (hit.distance <= 0.08f && CC.phasing == true)
+            {
+                //Add particle and sound effects
+            }
+        }
+    }
+
+
+}
+
